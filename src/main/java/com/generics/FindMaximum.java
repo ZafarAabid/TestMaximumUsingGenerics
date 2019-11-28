23cea8b36a659d4797e33ce3368b53c76310fe71
zafaraabid <zafaraabid3295@gmail.com>
2019-11-28 18:22:24 +0530

    Refactored code, created Generic class to take 3paramters to constructor

--- src/main/java/com/generics/FindMaximum.java
+++ src/main/java/com/generics/FindMaximum.java
@@ -1,9 +1,24 @@
 package com.generics;
 
-public class FindMaximum {
+public class FindMaximum<E extends Comparable> {
+        E firstValue;
+        E secoundValue;
+        E thirdValue;
 
+    public FindMaximum(E firstValue, E secoundValue, E thirdValue) {
+        this.firstValue = firstValue;
+        this.secoundValue = secoundValue;
+        this.thirdValue = thirdValue;
+    }
+
+    public <E extends Comparable> E findLargest() {
+
+        E max=(E) findLargest(firstValue,secoundValue,thirdValue);
+        return max;
+    }
     public static <E extends Comparable<E>> E findLargest(E firstValue, E secoundValue, E thirdValue) {
         E max=firstValue;
+
         if(secoundValue.compareTo(max)>0)
             max=secoundValue;
         if(thirdValue.compareTo(max)>0) {
@@ -11,4 +26,7 @@ public class FindMaximum {
         }
         return max;
     }
+
+
+
 }
--- src/test/java/com/generics/MaximumNumberTests.java
+++ src/test/java/com/generics/MaximumNumberTests.java
@@ -1,6 +1,5 @@
 package com.generics;
 
-import jdk.nashorn.internal.objects.annotations.Constructor;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,6 +7,7 @@ public class MaximumNumberTests {
 
     @Test
     public void givenNumber_IfFindMaximum_ReturnThatIntegerValue() {
+        System.out.println("givenNumber_IfFindMaximum_ReturnThatIntegerValue");
 
         Integer largest=FindMaximum.findLargest(11,21,3);
         System.out.println("largest number is "+largest);
@@ -16,6 +16,7 @@ public class MaximumNumberTests {
 
     @Test
     public void givenNumbers_ifFindMaximum_ReturnThatFloatValue() {
+        System.out.println("givenNumbers_ifFindMaximum_ReturnThatFloatValue");
 
         Float largest=FindMaximum.findLargest(1.1f,21.1f,3.0f);
         System.out.println("largest number is "+largest);
@@ -24,9 +25,40 @@ public class MaximumNumberTests {
 
     @Test
     public void givenNumbers_ifFindMaximum_ReturnThatStringValue() {
+        System.out.println("givenNumbers_ifFindMaximum_ReturnThatStringValue");
+
         String largest=FindMaximum.findLargest("aaz","sadfg","qwer");
         System.out.println("largest number is "+largest);
         Assert.assertSame("sadfg",largest);
+    }
+
+    @Test
+    public void givenNumbers_ifFindMaximum_ReturnThatIntegerValue_UsingConstructor() {
+        System.out.println("givenNumbers_ifFindMaximum_ReturnThatIntegerValue_UsingConstructor");
+
+        FindMaximum findMaximum = new FindMaximum<Integer>(456,85,10);
+        Integer largest = (Integer) findMaximum.findLargest();
+        System.out.println("largest number is "+largest);
+        Assert.assertEquals(new Integer(456),largest);
+    }
+    @Test
+    public void givenNumbers_ifFindMaximum_ReturnThatStringValue_UsingConstructor() {
+        System.out.println("givenNumbers_ifFindMaximum_ReturnThatStringValue_UsingConstructor");
+
+        FindMaximum findMaximum = new FindMaximum<String>("asd","qwe","zxc");
+        String largest = (String) findMaximum.findLargest();
+        System.out.println("largest number is "+largest);
+        Assert.assertEquals("zxc",largest);
+    }
+
+    @Test
+    public void givenNumbers_ifFindMaximum_ReturnThatFloatValue_UsingConstructor() {
+        System.out.println("givenNumbers_ifFindMaximum_ReturnThatFloatValue_UsingConstructor");
+
+        FindMaximum findMaximum = new FindMaximum<Float>(1.1f,20.0f,151.1f);
+        Float largest = (Float) findMaximum.findLargest();
+        System.out.println("largest number is "+largest);
+        Assert.assertEquals(new Float(151.1f),largest,0.1);
     }
 
 
